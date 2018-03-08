e04ec3abc24ea41e612e2dba2ad5e3a71f670b5a
Nicholas Kute <heavybitent@gmail.com>
2018-03-08 13:46:28 +0300

    new changes

--- resources/views/admin/manage/ebooks/edit.blade.php
+++ resources/views/admin/manage/ebooks/edit.blade.php
@@ -99,7 +99,7 @@
                                               Ebook PDF
                                           </label>
                                           <div class="col-sm-9">
-                                            <a href="{{ base_path() }}/{{ $ebook->src }}" target="_blank">Current Ebook PDF - Click To View</a>
+                                            <a href="{{ env('app.url') }}{{ $ebook->src }}" target="_blank">Current Ebook PDF - Click To View</a>
                                             <br><br>
                                             Select a replacement PDF below:
                                             <br>
