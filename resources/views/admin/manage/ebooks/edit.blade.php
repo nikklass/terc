e25adef02472e26ed3084540e7b312490431f904
Nicholas Kute <heavybitent@gmail.com>
2018-03-08 14:08:40 +0300

    new changes

--- resources/views/admin/manage/ebooks/edit.blade.php
+++ resources/views/admin/manage/ebooks/edit.blade.php
@@ -96,12 +96,12 @@
                                        <div  class="form-group{{ $errors->has('uploadfile') ? ' has-error' : '' }}">
                                               
                                           <label for="uploadfile" class="col-sm-3 control-label">
-                                              Ebook PDF zzz <br>
-                                              {!!env('app.url') !!}
+                                              Ebook PDF <br>
+                                              {!! config('app.url') !!}
                                           </label>
                                           <div class="col-sm-9">
                                           
-                                            <a href="{!! env('app.url') !!}{{ $ebook->src }}" target="_blank">Current Ebook PDF - Click To View</a>
+                                            <a href="{!! config('app.url') !!}{{ $ebook->src }}" target="_blank">Current Ebook PDF - Click To View</a>
                                             <br><br>
                                             Select a replacement PDF below:
                                             <br>
