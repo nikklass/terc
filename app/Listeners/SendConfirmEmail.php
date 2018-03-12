25936a74011a410432a81d172575b224cd515492
Nicholas Kute <heavybitent@gmail.com>
2018-03-12 16:30:01 +0300

    new changes

--- app/Listeners/SendConfirmEmail.php
+++ app/Listeners/SendConfirmEmail.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\UserCreated;
 use App\Mail\NewUserConfirm;
+use Dingo\Api\Exception\StoreResourceFailedException;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Mail;
